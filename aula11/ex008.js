2b19550daeb2adcff1c5b6bf3e849f21f0b85784
Leo Jatahy <118844419+leojatahy@users.noreply.github.com>
2023-02-14 23:35:33 -0400

    a

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro e ${vel}kml/h`)
+if(vel > 60){//Condição Simple
+    console.log(`limite da via ultrapassado você foi MULTADO`)
+}
+console.log('Respeite as leis de transito!')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var aluno = '5.5'
+if(aluno >= '7.5'){
+    console.log('aprovado')
+}else{
+    console.log('reprovado')
+}
