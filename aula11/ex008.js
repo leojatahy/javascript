cd794b29c18fe41fed78da9ec055c0d1fbefcd05
Leo Jatahy <118844419+leojatahy@users.noreply.github.com>
2023-03-31 18:00:18 -0400

    AULA 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 72
+    console.log(`a sua velocidade atua e de ${vel} kml/h`)
+if (vel > 60) { //condição SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'italia'
+console.log(`Vivendo na ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você e Brasileiro')
+}else {
+    console.log(`Você e Estrangeiro!`)
+}
