59b2ed86a58531f7ab0da20a7923b06b40cc7bb4
Leo Jatahy <118844419+leojatahy@users.noreply.github.com>
2023-04-05 17:28:50 -0400

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -41,7 +41,7 @@ function verificar() {
         img.setAttribute('src', '../fotos/senhora.jpg')
       }
     }
-    res.innerHTML = `Detectamos ${gênero} com a idade ${idade}`
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos !`
     res.appendChild(img) 
   }
 }
