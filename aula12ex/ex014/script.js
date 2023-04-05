9545f120f28168fd29d64a6ee8d602ea35422caa
Leo Jatahy <118844419+leojatahy@users.noreply.github.com>
2023-04-05 16:29:49 -0400

    depois de muito sofrimento consegui fazer 12 ex

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,24 @@
+function carregar () {
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#foto')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = `../fotos/manha.jpg`
+        document.body.style.background =`#748A8C`
+
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde
+        img.src = '../fotos/tarde.jpg'  
+        document.body.style.background =`#BF4A3F`
+    } else {
+        // boa noite
+        img.src = '../fotos/noite.jpg'
+        document.body.style.background =`#142426`
+    }  
+
+}
+
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,47 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.querySelector('div#res')
+  if (fano.value.length == 0 || Number(fano.value) > ano)  {
+    window.alert('[ERRO] Verifique os dados e tente novamente!')
+  }else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.querySelector ('#img')
+    if (fsex[0].checked) {
+      gênero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', '../fotos/bebem.jpg')
+      }else if (idade < 21) {
+      // jovem
+      img.setAttribute('src', '../fotos/jovemm.jpg')
+      }else if (idade < 50) {
+        //adulto
+        img.setAttribute('src', '../fotos/homem.jpg')
+      }else {
+        //idoso
+        img.setAttribute('src', '../fotos/velho.jpg')
+      }
+    }else if (fsex[1].checked) {
+      gênero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        //criança
+        img.setAttribute('src', '../fotos/bebef.jpg')
+      }else if (idade < 21) {
+      // jovem
+      img.setAttribute('src', '../fotos/jovemf.jpg')
+      }else if (idade < 50) {
+        //adulto
+        img.setAttribute('src', '../fotos/mulher.jpg')
+      }else {
+        //idoso
+        img.setAttribute('src', '../fotos/senhora.jpg')
+      }
+    }
+    res.innerHTML = `Detectamos ${gênero} com a idade ${idade}`
+    res.appendChild(img) 
+  }
+}
