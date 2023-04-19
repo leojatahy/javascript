a44f6e1b711039ea8a90b02e587c784aa43d3570
Leo Jatahy <118844419+leojatahy@users.noreply.github.com>
2023-04-19 09:36:57 -0400

    fim do curso  de JavaScript mais nao o final dos estudos

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -1,7 +1,8 @@
-function fatorial(n){
-    if(n == 1){
-        return 1
-    } else {
-        return n* fatorial(n-1)
-    }
-}
+ function fatorial(n){
+     if(n == 1){
+         return 1
+     } else {
+         return n* fatorial(n-1)
+     }
+ }
+ 
